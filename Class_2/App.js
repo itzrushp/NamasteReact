bcebf47ffd473b47e03e61b4868f173484945f74
Rush <itzashert@gmail.com>
2025-04-24 12:47:03 +0530

    Class 2

--- Class_2/App.js
+++ Class_2/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading1 = React.createElement('h1', {
     id: 'heading1', style: {
         background: 'blue',
+        key: 0,
     }, className: 'headingggs',
 }, "Heading 1 ");
 
-const heading2 = React.createElement('h2', { id: 'heading2' }, "Heading 2 ");
+const heading2 = React.createElement('h2', { id: 'heading2' , key : 1 }, "Heading 2 ");
 
-const container = React.createElement('div', { id: 'container' }, [heading1, heading2]);
+const container = React.createElement('div', { id: 'container' , key : 2}, [heading1, heading2]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(container);
